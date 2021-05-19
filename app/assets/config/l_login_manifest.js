efcaccef0239c039d5023a8de0f71be382433b6d
SatoshiTagomori <satoshi@expresstutor.tokyo>
2021-05-20 08:27:06 +0900

    0.1.0.15

--- app/assets/config/l_login_manifest.js
+++ app/assets/config/l_login_manifest.js
@@ -1,1 +1,2 @@
 //= link_directory ../stylesheets/l_login .css
+
--- lib/l_login.rb
+++ lib/l_login.rb
@@ -2,5 +2,27 @@ require "l_login/version"
 require "l_login/engine"
 
 module LLogin
-  # Your code goes here...
+    
+  def self.tes()
+     HtReq.test_send({
+         :url=>self.login_url
+     }) 
+  end
+
+  def self.login_url
+     session[:line_token]='abc123'
+     HtReq.SetGetParams(
+            'GET',
+            URI.parse('https://access.line.me/oauth2/v2.1/authorize'),
+            {
+                :params=>{
+                    :response_type=>'code',
+                    :client_id=>ENV['LINE_CLIENT_ID'],
+                    :redirect_uri=>ENV['LINE_REDIRECT_URL'],
+                    :state=>'abc123',
+                    :scope=>ENV['LINE_SCOPE']
+                }
+            }
+         ).to_s
+  end
 end
